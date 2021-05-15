989568299aaa3610e4dc9b2b645432223657bbfa
amazinglzy <1075749478@qq.com>
2021-05-15 11:05:12 +0800

    add lowlight by bracket

--- README.md
+++ README.md
@@ -1,65 +1,32 @@
 # lowlight-block README
 
-This is the README for your extension "lowlight-block". After writing up a brief description, we recommend including the following sections.
+lowlight-block is intended to low light not necessary code for better code reading and development. For example, 
+
+```cpp
+Status s = f(arg1, arg2, arg3);
+LOG(WARNING) << s.ToString();
+```
+
+the extension is intended to low light code like `LOG(WARNING) << s.ToString()`. Not just logging, the logic code changing from time to time, we may low light some code that is not important and focus on the main.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Currently, the extension support:
 
-For example if there is an image subfolder under your extension project workspace:
+1. search code by regex, then find range depends on `(),{},[]`, and lowlight the codd
 
-\!\[feature X\]\(images/feature-x.png\)
+```
+{
+    "lowlight-block.rules": [
+        "LOG",
+        "console.log"
+    ]
+}
+```
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+![bracket example](images/bracket_example.png)
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
 
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+* `lowlight-block.rules`: currently a list of string to match
--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,109 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let cachedType: vscode.TextEditorDecorationType | undefined;
+	function setOpacity(opacity: string): vscode.TextEditorDecorationType | undefined {
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			let ranges: vscode.Range[] = [];
+			let text = editor.document.getText();
+			let matchpoints: string[] | undefined = vscode.workspace.getConfiguration('lowlight-block').get('rules');
+			if (matchpoints) {
+				for (let point of matchpoints) {
+					let regex = new RegExp(point, 'g');
+					let match;
+					while (match = regex.exec(text)) {
+						let range = getRange(match, editor.document);
+						if (range) {
+							ranges.push(range);
+						}
+					}
+				}
+			}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lowlight-block" is now active!');
+			let options: vscode.DecorationRenderOptions = {
+				opacity: opacity,
+				isWholeLine: true
+			};
+
+			if (cachedType) {
+				cachedType.dispose();
+			}
+
+			cachedType = vscode.window.createTextEditorDecorationType(options);
+			editor.setDecorations(cachedType, ranges);
+			return cachedType;
+		}
+	}
+
+	var timeout: NodeJS.Timer;
+	function trigger() {
+		if (timeout) { clearTimeout(timeout); }
+		timeout = setTimeout(() => setOpacity('0.1'), 200);
+	}
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lowlight-block.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('lowlight-block.lowlight', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Lowlight Block!');
+		trigger();
 	});
 
 	context.subscriptions.push(disposable);
+
+	vscode.window.onDidChangeActiveTextEditor(
+		editor => {
+			if (editor) {
+				trigger();
+			}
+		},
+		null,
+		context.subscriptions
+	);
+
+	vscode.workspace.onDidChangeTextDocument(
+		event => {
+			var activeEditor = vscode.window.activeTextEditor;
+			if (activeEditor && event.document === activeEditor.document) {
+				trigger();
+			}
+		},
+		null,
+		context.subscriptions
+	);
+
+	trigger();
 }
+
+
+function getRange(match: RegExpExecArray, document: vscode.TextDocument): vscode.Range | undefined {
+	const openChars = new Map([['{', '}'], ['(', ')'], ['[', ']']]);
+	const closeChars = new Map([['}', '{'], [')', '('], [']', '[']]);
+
+	let text = document.getText();
+	let state: string[] = [];
+	for (let cursor = match.index + match[0].length; cursor < text.length; ++ cursor) {
+		let c = text.charAt(cursor)
+		if (openChars.has(c)) {
+			state.push(c)
+		} else if (closeChars.has(c)) {
+			if (state.length > 0) {
+				let mc = state.pop();
+				if (mc == undefined || openChars.get(mc) != c) {
+					return;
+				}
+			} 
+			if (state.length == 0) {
+				let start = document.positionAt(match.index);
+				let end = document.positionAt(cursor);
+				return new vscode.Range(start, end);
+			}
+		}
+		// console.log(c, state)
+	}
+	return;
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
